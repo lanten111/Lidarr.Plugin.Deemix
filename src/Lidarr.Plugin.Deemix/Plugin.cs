c4782eb3efee0631bb2819aae2733dbe6db5bf0d
muhumbulo <makhadolucky@gmail.com>
2026-02-08 19:33:29 +0200

    remove artist from name

--- src/Lidarr.Plugin.Deemix/Plugin.cs
+++ src/Lidarr.Plugin.Deemix/Plugin.cs
@@ -5,7 +5,7 @@ namespace NzbDrone.Core.Plugins
     public class DeemixPlugin : Plugin
     {
         public override string Name => "Deemix";
-        public override string Owner => "ta264";
-        public override string GithubUrl => "https://github.com/ta264/Lidarr.Plugin.Deemix";
+        public override string Owner => "lanten111";
+        public override string GithubUrl => "https://github.com/lanten111/Lidarr.Plugin.Deemix";
     }
 }
